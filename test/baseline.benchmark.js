d07f13773dea2b8b38239ee1306971f4bbf843c9
Mike McNeil <mikermcneil@users.noreply.github.com>
2016-12-20 16:57:47 -0600

    Finish up benchmark reports and commenting out unnecessary stuff.

--- test/baseline.benchmark.js
+++ test/baseline.benchmark.js
@@ -4,7 +4,7 @@
 
 var parley = require('../');
 var benchSync = require('./utils/bench-sync.util');
-var bench = require('./utils/bench.util');
+// var bench = require('./utils/bench.util');
 
 
 
@@ -41,20 +41,31 @@ describe('baseline.benchmark.js', function() {
   //  ╩ ╩╩╚═╝ ╩ ╚═╝╩╚═╩╚═╝╩ ╩╩═╝  ╩╚═╚═╝╩  ╚═╝╩╚═ ╩ ╚═╝
   // Just some one-off samples.
   //
+  //
+  // Dec 20, 2016 (take 2):  (notice how the additional time added by calling .exec() is actually a negative number)
+  // ================================================================================================================
+  //  baseline.benchmark.js
+  //    parley(handler)
+  // • just_build#0 x 22,540 ops/sec ±3.15% (80 runs sampled)
+  //      ✓ should be performant enough (using benchSync())
+  //    parley(handler).exec(cb)
+  // • build_AND_exec#0 x 22,743 ops/sec ±1.77% (83 runs sampled)
+  // ================================================================================================================
+  //
   // Dec 20, 2016:
   // ================================================================================================================
   //  baseline.benchmark.js
-  //    parley(handleExec)
-  // • parley(handleExec)#0 x 23,432 ops/sec ±2.95% (79 runs sampled)
+  //    parley(handler)
+  // • parley(handler)#0 x 23,432 ops/sec ±2.95% (79 runs sampled)
   //      ✓ should be performant enough (using benchSync())
-  //    parley(handleExec).exec(cb)
-  // • parley(handleExec).exec(cb)#0 x 22,959 ops/sec ±3.83% (78 runs sampled)
+  //    parley(handler).exec(cb)
+  // • parley(handler).exec(cb)#0 x 22,959 ops/sec ±3.83% (78 runs sampled)
   //      ✓ should be performant enough (using benchSync())
-  // • parley(handleExec).exec(cb)#0 x 19,548 ops/sec ±2.04% (72 runs sampled)
+  // • parley(handler).exec(cb)#0 x 19,548 ops/sec ±2.04% (72 runs sampled)
   //      ✓ should be performant enough (using bench())
-  // • parley(handleExec).exec(cb) + artificial setImmediate in handler#0 x 19,135 ops/sec ±1.73% (73 runs sampled)
+  // • parley(handler).exec(cb) + artificial setImmediate in handler#0 x 19,135 ops/sec ±1.73% (73 runs sampled)
   //      ✓ should be performant enough (using bench())
-  // • parley(handleExec).exec(cb) + artificial setImmediate wrapped around the whole thing#0 x 18,686 ops/sec ±6.00% (71 runs sampled)
+  // • parley(handler).exec(cb) + artificial setImmediate wrapped around the whole thing#0 x 18,686 ops/sec ±6.00% (71 runs sampled)
   //      ✓ should be performant enough (using bench())
   // ================================================================================================================
 
@@ -63,12 +74,12 @@ describe('baseline.benchmark.js', function() {
   //  ╔═╗╦ ╦╦╔╦╗╔═╗
   //  ╚═╗║ ║║ ║ ║╣
   //  ╚═╝╚═╝╩ ╩ ╚═╝
-  describe('parley(handleExec)', function(){
+  describe('parley(handler)', function(){
     it('should be performant enough (using benchSync())', function (){
-      benchSync('parley(handleExec)', [
+      benchSync('parley(handler)', [
 
         // •~ between 21,000 and 23,500 ops/sec  (Dec 20, 2016)
-        function (){
+        function just_build(){
           var deferred = parley(function(handlerCb) { return handlerCb(); });
         }
 
@@ -76,14 +87,14 @@ describe('baseline.benchmark.js', function() {
     });
   });
 
-  describe('parley(handleExec).exec(cb)', function(){
+  describe('parley(handler).exec(cb)', function(){
     it('should be performant enough (using benchSync())', function (){
-      benchSync('parley(handleExec).exec(cb)', [
+      benchSync('parley(handler).exec(cb)', [
 
         // •~ between 21,000 and 23,000 ops/sec  (Dec 20, 2016)
         //    --SAME AS ABOVE, basically -- like 200-500 ops/second slower, tops --
         //    (and keep in mind that's below the % error margin)
-        function (){
+        function build_AND_exec(){
           var deferred = parley(function(handlerCb) { return handlerCb(); });
           deferred.exec(function (err) {
             if (err) {
@@ -96,55 +107,61 @@ describe('baseline.benchmark.js', function() {
 
       ]);//</benchSync()>
     });
-    it('should be performant enough (using bench())', function (done){
-      bench('parley(handleExec).exec(cb)', [
 
-        // •~ between 18,000 and 19,500 ops/sec  (Dec 20, 2016)
-        //    --the extra delay vs. the one directly above is probably
-        //    due to the setImmediate we're using in the test suite--
-        function (next){
-          var deferred = parley(function(handlerCb) { return handlerCb(); });
-          deferred.exec(function (err) {
-            if (err) { return next(err); }
-            return next();
-          });
-        }
+    // Not particularly relevant:
+    // (the extra setImmediate just adds weight and makes it harder to judge results)
+    // =================================================================================
+    // it('should be performant enough (using bench())', function (done){
+    //   bench('parley(handler).exec(cb)', [
 
-      ], done);//</bench()>
-    });
-    it('should be performant enough (using bench())', function (done){
-      bench('parley(handleExec).exec(cb) + artificial setImmediate in handler', [
+    //     // •~ between 18,000 and 19,500 ops/sec  (Dec 20, 2016)
+    //     //    --the extra delay vs. the one directly above is probably
+    //     //    due to the setImmediate we're using in the test suite--
+    //     function (next){
+    //       var deferred = parley(function(handlerCb) { return handlerCb(); });
+    //       deferred.exec(function (err) {
+    //         if (err) { return next(err); }
+    //         return next();
+    //       });
+    //     }
 
-        // •~ between 18,000 and 19,500 ops/sec  (Dec 20, 2016)
-        //    --this is EXACTLY THE SAME AS THE ONE DIRECTLY ABOVE, basically --
-        //    it is consistently <~400 ops/second slower, sometimes only like 20ops slower --
-        function (next){
-          var deferred = parley(function(handlerCb) { setImmediate(function(){ return handlerCb(); }); });
-          deferred.exec(function (err) {
-            if (err) { return next(err); }
-            return next();
-          });
-        }
+    //   ], done);//</bench()>
+    // });
+    // it('should be performant enough (using bench())', function (done){
+    //   bench('parley(handler).exec(cb) + artificial setImmediate in handler', [
 
-      ], done);//</bench()>
-    });
-    it('should be performant enough (using bench())', function (done){
-      bench('parley(handleExec).exec(cb) + artificial setImmediate wrapped around the whole thing', [
+    //     // •~ between 18,000 and 19,500 ops/sec  (Dec 20, 2016)
+    //     //    --this is EXACTLY THE SAME AS THE ONE DIRECTLY ABOVE, basically --
+    //     //    it is consistently <~400 ops/second slower, sometimes only like 20ops slower --
+    //     function (next){
+    //       var deferred = parley(function(handlerCb) { setImmediate(function(){ return handlerCb(); }); });
+    //       deferred.exec(function (err) {
+    //         if (err) { return next(err); }
+    //         return next();
+    //       });
+    //     }
 
-        // •~ between 18,000 and 19,500 ops/sec  (Dec 20, 2016)
-        //    -- ALSO THE SAME, WITH THE SAME SLIGHT VARIATIONS --
-        function (next){
-          setImmediate(function(){
-            var deferred = parley(function(handlerCb) { return handlerCb(); });
-            deferred.exec(function (err) {
-              if (err) { return next(err); }
-              return next();
-            });
-          });
-        }
+    //   ], done);//</bench()>
+    // });
+    // it('should be performant enough (using bench())', function (done){
+    //   bench('parley(handler).exec(cb) + artificial setImmediate wrapped around the whole thing', [
 
-      ], done);//</bench()>
-    });
-  });
+    //     // •~ between 18,000 and 19,500 ops/sec  (Dec 20, 2016)
+    //     //    -- ALSO THE SAME, WITH THE SAME SLIGHT VARIATIONS --
+    //     function (next){
+    //       setImmediate(function(){
+    //         var deferred = parley(function(handlerCb) { return handlerCb(); });
+    //         deferred.exec(function (err) {
+    //           if (err) { return next(err); }
+    //           return next();
+    //         });
+    //       });
+    //     }
 
-});//</describe>
+    //   ], done);//</bench()>
+    // });
+    // =================================================================================
+
+  });//</ describe: parley(handler().exec(cb) )
+
+});//</describe (top-level) >
