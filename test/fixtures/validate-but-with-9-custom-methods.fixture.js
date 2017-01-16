77db922514ccc33ec5d404a50766c0f8a6841112
Mike McNeil <mikermcneil@users.noreply.github.com>
2017-01-15 20:55:38 -0600

    Simplify new 9-custom-methods fixture to eliminate variables impacting performance.

--- test/fixtures/validate-but-with-9-custom-methods.fixture.js
+++ test/fixtures/validate-but-with-9-custom-methods.fixture.js
@@ -19,30 +19,13 @@ var parley = require('../../');
  *
  * @returns {Deferred} If no callback specified
  */
-module.exports = function validateButWith9CustomMethods( /* variadic */ ){
-
-  var metadata = {};
-
-  var explicitCbMaybe;
-
-  // Handle variadic usage:
-  // ===========================================================================
-  if (!_.isUndefined(arguments[0])) {
-    explicitCbMaybe = arguments[0];
-  }//>-
-  // ===========================================================================
-
-  // This deferred may or may not actually need to get built.
-  // (but in case it does, we define it out here so we can unambiguously
-  // return it below)
-  var deferred;
-
+module.exports = function validateButWith9CustomMethods(explicitCbMaybe){
 
   // If an explicit callback was specified, then go ahead
   // and proceed to where the real action is at.
   // Otherwise, no callback was specified explicitly,
   // so we'll build and return a Deferred instead.
-  deferred = parley(function (finalCb){
+  return parley(function (finalCb){
 
     // Now actually do stuff.
     // ...except actually don't-- this is just pretend.
@@ -51,30 +34,16 @@ module.exports = function validateButWith9CustomMethods( /* variadic */ ){
     return finalCb();
 
   }, explicitCbMaybe, {
-    a: function (beep, boop) { console.log(Math.random()+'hi0'); return deferred; },
-    b: function (baa, baaa, black, sheep) { console.log(Math.random()+'hi1'); return deferred; },
-    c: function (beep, boop) { console.log(Math.random()+'hi2'); return deferred; },
-    d: function (baa, baaa, black, sheep) { console.log(Math.random()+'hi3'); return deferred; },
-    e: function (beep, boop) { console.log(Math.random()+'hi5'); return deferred; },
-    f: function (baa, baaa, black, sheep) { console.log(Math.random()+'hi5'); return deferred; },
-    g: function (beep, boop) { console.log(Math.random()+'hi6'); return deferred; },
-    h: function (baa, baaa, black, sheep) { console.log(Math.random()+'hi7'); return deferred; },
-    i: function (beep, boop) { console.log(Math.random()+'hi8'); return deferred; },
+    a: function (beep, boop) { console.log(Math.random()+'hi0'); return this; },
+    b: function (baa, baaa, black, sheep) { console.log(Math.random()+'hi1'); return this; },
+    c: function (beep, boop) { console.log(Math.random()+'hi2'); return this; },
+    d: function (baa, baaa, black, sheep) { console.log(Math.random()+'hi3'); return this; },
+    e: function (beep, boop) { console.log(Math.random()+'hi5'); return this; },
+    f: function (baa, baaa, black, sheep) { console.log(Math.random()+'hi5'); return this; },
+    g: function (beep, boop) { console.log(Math.random()+'hi6'); return this; },
+    h: function (baa, baaa, black, sheep) { console.log(Math.random()+'hi7'); return this; },
+    i: function (beep, boop) { console.log(Math.random()+'hi8'); return this; },
   });
-
-
-  // If we ended up building a Deferred above, we would have done so synchronously.
-  // In other words, if there's going to be a Deferred, we have it here.
-  //
-  // So if we DON'T have a Deferred, then we know that we must have already went ahead
-  // and performed our business logic.  So we'll just return undefined.
-  if (!deferred) {
-    return;
-  }//-•
-
-  // When we're confident that our Deferred is ready for primetime,
-  // we finish up by returning it.
-  return deferred;
 
 };
 
